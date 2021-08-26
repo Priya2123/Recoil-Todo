d0ffe1503f254a67e987016432e9a9d4af376fb5
Priya2123 <aggarwalpriya1121@gmail.com>
2021-08-27 01:16:27 +0530

    WIP

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { Grid, Typography } from "@material-ui/core";
 import { useStyles } from "./LandingStyles";
+import Lottie from "react-lottie";
+import animation from "./animation.json";
 
 const Landing = () => {
   const classes = useStyles();
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
     <>
       <Grid container lg={12} md={12} style={{ marginTop: "5%" }}>
@@ -13,6 +25,14 @@ const Landing = () => {
               <Typography className={classes.text} variant="h3">
                 Oof, just Tasks.
               </Typography>
+            </Grid>
+          </Grid>
+          <Grid container justify="center" lg={12} md={12}>
+            <Grid item lg={12} md={12}>
+              <Lottie
+                //   style={{ height: "100%" }}
+                options={defaultOptions}
+              />
             </Grid>
           </Grid>
         </Grid>
